fb5356dfd962dbacd89d70cea670301fcba25fc9
Michael Mwangi <michaelmichire2016@gmail.com>
2024-04-07 07:14:21 -0700

    task #0 but by using Puppet

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,59 +1,4 @@
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-}
-
-file { '/data':
-  ensure  => 'directory'
-}
-
-file { '/data/web_static':
-  ensure => 'directory'
-}
-
-file { '/data/web_static/releases':
-  ensure => 'directory'
-}
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory'
-}
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-}
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
-}
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-}
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
-
-file { '/var/www':
-  ensure => 'directory'
-}
-
-file { '/var/www/html':
-  ensure => 'directory'
-}
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "This is my first upload  in /var/www/index.html***\n"
-}
-
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page - Error page\n"
-}
+# Puppet for the task 0
 
 $nginx_conf = "server {
     listen 80 default_server;
@@ -74,12 +19,54 @@ $nginx_conf = "server {
       internal;
     }
 }"
-
-file { '/etc/nginx/sites-available/default':
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+}
+-> file { '/data':
+  ensure  => 'directory'
+}
+-> file { '/data/web_static':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
+}
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
+}
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+file { '/var/www':
+  ensure => 'directory'
+}
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "This is my first upload  in /var/www/index.html***\n"
+}
+-> file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page - Error page\n"
+}
+-> file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
 }
-
-exec { 'nginx restart':
+-> exec { 'nginx restart':
   path => '/etc/init.d/'
 }
