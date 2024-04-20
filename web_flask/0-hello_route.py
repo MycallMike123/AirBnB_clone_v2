855dd22c161286e9666668bf51e9870506bec95f
Michael Mwangi <michaelmichire2016@gmail.com>
2024-04-20 00:35:16 -0700

    A script that starts a Flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+This script starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000.
+Routes:
+    /: display “Hello HBNB!”
+The option strict_slashes=False is used in route definition.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Display "Hello HBNB!" on the index page."""
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
